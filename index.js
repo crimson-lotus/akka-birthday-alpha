a7b3bb745633cc2b5aa7a8157dbefde2e9fc1bcb
Nitish C <nitishc@Nitishs-MacBook-Air.local>
2022-06-05 01:05:55 +0530

    Added extra styling and refined the code a bit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 $('#background-div').hide()
+$('.second').hide()
+$('.third').hide()
+$('.fourth').hide()
+$('.fifth').hide()
+$('.sixth').hide()
+$('.seventh').hide()
+$('.eigth').hide()
 
 $('.switch').click(function() {
     $('#initial-div').hide()
-    $('#background-div').fadeIn(500)
+    $('#background-div').fadeIn(600)
 })
