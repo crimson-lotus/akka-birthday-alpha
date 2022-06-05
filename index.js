6ffe994a8c9914843b4d631cb449e85b0e64cb71
Nitish C <nitishc@Nitishs-MacBook-Air.local>
2022-06-05 20:40:08 +0530

    Final (Aplha) commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $('#background-div').hide()
+$('.first').hide()
 $('.second').hide()
 $('.third').hide()
 $('.fourth').hide()
@@ -6,8 +7,20 @@ $('.fifth').hide()
 $('.sixth').hide()
 $('.seventh').hide()
 $('.eigth').hide()
+$('.ninth').hide()
+var child = 2;
 
 $('.switch').click(function() {
     $('#initial-div').hide()
-    $('#background-div').fadeIn(600)
+    $('#background-div').fadeIn(300)
+    // $('.ninth').show()
+    $('.first').fadeIn(1000)
+    setInterval(function() { 
+        $(".content").show().not(".content:nth-child(" + child + ")").hide(); 
+        if (child === 9) { 
+        //   child = 2; 
+        } else { 
+          child++; 
+        } 
+      }, 4000);
 })
